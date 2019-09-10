a4b89e655892adffadef5ab27ee4465f6733eeaf
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-10 11:02:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,9 +14,15 @@ new_array
 end
 
 def assign_rooms (array)
-new_array = []
   array.each_with_index.collect do |name, index|
-    new_array << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    "Hello, #{name}! You'll be assigned to room #{index + 1}!"
   end
-  p new_array
 end
+
+def printer (array)
+  batch_badge_creator.each_with_index do |name, index|
+    puts name
+    puts assign_rooms[index]
+  end
+end
+
